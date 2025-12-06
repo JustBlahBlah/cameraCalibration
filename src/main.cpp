80b2427e9acd85562b3454cd9de51bbf56c1d513
Yu <realhikkiboy@gmail.com>
2025-12-06 21:14:22 +0000

    Add cmera calibration and undistortion logic

--- src/main.cpp
+++ src/main.cpp
@@ -50,6 +50,17 @@ int main(int argc, char** argv) {
 	
 	cv::Mat frame, gray;
 
+	std::vector<std::vector<cv::Point2f>> allCorners;
+	std::vector<std::vector<cv::Point3f>> objectPoints;
+
+	std::vector<cv::Point3f> obj;
+    	for (int y = 0; y < patternsize.height; y++)
+        	for (int x = 0; x < patternsize.width; x++)
+            		obj.emplace_back(x * squareSize, y * squareSize, 0.0f);
+	
+	cv::Mat cameraMatrix, distCoeffs;
+    	bool calibrated = false;
+
 	while(true){
 		cap >> frame;
 		if (frame.empty()) {
@@ -65,14 +76,57 @@ int main(int argc, char** argv) {
         	bool patternfound = cv::findChessboardCorners(gray, patternsize, corners, cv::CALIB_CB_ADAPTIVE_THRESH | cv::CALIB_CB_NORMALIZE_IMAGE | cv::CALIB_CB_FAST_CHECK);
 
         	if(patternfound) {
-
 			cv::cornerSubPix(gray, corners, cv::Size(11, 11), cv::Size(-1, -1), cv::TermCriteria(cv::TermCriteria::EPS + cv::TermCriteria::COUNT, 30, 0.1));
 			cv::drawChessboardCorners(frame, patternsize, cv::Mat(corners), patternfound);
         	}
+		
+		cv::Mat display;
+        	if (calibrated) {
+			cv::Mat und;
+			cv::undistort(frame, und, cameraMatrix, distCoeffs);
+            		if (und.size() != frame.size()) cv::resize(und, und, frame.size());
+			cv::hconcat(frame, und, display);
+        	} else {
+			cv::Mat placeholder = frame.clone();
+			cv::putText(placeholder, "Undistorted (off)", cv::Point(10, 30),
+                    	cv::FONT_HERSHEY_SIMPLEX, 0.8, cv::Scalar(0, 0, 255), 2);	
+			cv::putText(frame, "press <c> to capture frame if parren is found", cv::Point(10, 30), cv::FONT_HERSHEY_SIMPLEX,
+               	 		0.8, cv::Scalar(0, 255, 0), 2);
+			cv::putText(frame, "press <k> to calibrate camera", cv::Point(10, 50), cv::FONT_HERSHEY_SIMPLEX,
+               	 		0.8, cv::Scalar(0, 255, 0), 2); 
 
-		cv::imshow("live", frame);
+			cv::hconcat(frame, placeholder, display);
+        	}
 
-		if(cv::waitKey(10) == 27) break;
+
+		int key = cv::waitKey(1);
+
+		if (key == 'c') {
+            		if (patternfound) {
+                		allCorners.push_back(corners);
+                		objectPoints.push_back(obj);
+				std::cout << "captured\n";
+            		} else {
+				std::cout << "Chessboard not found — try again\n";
+            		}
+        	} else if (key == 'k') {
+            		if (allCorners.size() < 5) {
+				std::cout << "Need at least 5 frames. Currently: "<< allCorners.size() << std::endl;
+            		} else {
+				std::vector<cv::Mat> rvecs, tvecs;
+				std::cout << "Running calibration...\n";
+                		double rms = cv::calibrateCamera(objectPoints, allCorners,
+                                             gray.size(), cameraMatrix,
+                                             distCoeffs, rvecs, tvecs);
+				std::cout << "RMS error = " << rms << std::endl;
+				std::cout << "Camera matrix:\n" << cameraMatrix << std::endl;
+				std::cout << "Distortion:\n" << distCoeffs << std::endl;
+                		calibrated = true;
+            		}
+        	} else if(key == 'q') break;
+
+		cv::imshow("live", display);
+
 	}
 
 	return 0;
